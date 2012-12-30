11363f1c3239d3541a9832e0e4aacc8c3735515a
Larry Finn <larry@cube2media.com>
2012-12-30 13:55:55 -0800

    fixed partials

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,87 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  
+  helper :all
+  helper_method  :current_user, :current_user_session, :require_user, 
+  :require_no_user, :current_admin, :current_admin_session, :require_admin, 
+  :require_no_admin, :require_auth, :super_admin, :super?
+  
+  private
+  
+    def current_user_session
+      return @current_user_session if defined?(@current_user_session)
+      @current_user_session = UserSession.find
+    end
+
+    def current_user
+      return @current_user if defined?(@current_user)
+      @current_user = current_user_session && current_user_session.record
+    end
+    
+    def require_user
+      unless current_user
+        store_location
+        flash[:notice] = "You must be logged in to access this page"
+        redirect_to new_user_session_url
+        return false
+      end
+    end
+
+    def require_no_user
+      if current_user
+        store_location
+        flash[:notice] = "You must be logged out to access this page"
+        redirect_to account_url
+        return false
+      end
+    end
+  
+    def current_admin_session
+      return @current_admin_session if defined?(@current_admin_session)
+      @current_admin_session = AdminSession.find
+    end
+  
+    def current_admin
+      return @current_admin if defined?(@current_admin)
+      @current_admin = current_admin_session && current_admin_session.record
+    end
+
+    def require_admin
+      unless current_admin
+        flash[:notice] = "You must be logged in to access this page"
+        redirect_to new_admin_session_url
+        return false
+      end
+    end
+
+    def require_no_admin
+      if current_admin
+        flash[:notice] = "You must be logged out to access this page"
+        redirect_to root_path
+        return false
+      end
+    end
+
+    def require_auth
+      unless current_user || current_admin
+        store_location
+        flash[:notice] = "You must be logged in to access this page"
+        redirect_to new_user_session_url
+        return false
+      end
+    end
+  
+  protected
+
+    def super_admin
+      unless super?
+        return false
+      end
+    end
+
+    def super?
+      authenticate_or_request_with_http_basic do |username, password|
+        username == APP['username'] && password == APP['password']
+      end
+    end
 end
